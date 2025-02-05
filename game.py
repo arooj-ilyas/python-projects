019db80bf779766f9b547426c2fa26335f022a78
Arooj Ilyas <arooj-ilyas@hotmail.com>
2025-02-05 14:25:04 +0000

    VARIABLE: number_of_guesses created

--- game.py
+++ game.py
@@ -6,5 +6,8 @@ number = random.randint(1, 10)
 # prompt user to enter name
 player_name = input("Hello, What's your name?")
 
+# add variable to store number of guesses, iterate(+1) this value each time user guesses
+number_of_guesses = 0
+
 # print string with user_name in it
 print('okay! '+ player_name+ ' I am Guessing a number between 1 and 10:')
