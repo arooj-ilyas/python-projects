b934a23ca659c78240230950a0f8ab02a9af4ce7
Arooj Ilyas <arooj-ilyas@hotmail.com>
2025-02-05 14:23:51 +0000

    command added: string with user name from variable assigned above

--- game.py
+++ game.py
@@ -5,3 +5,6 @@ number = random.randint(1, 10)
 
 # prompt user to enter name
 player_name = input("Hello, What's your name?")
+
+# print string with user_name in it
+print('okay! '+ player_name+ ' I am Guessing a number between 1 and 10:')
